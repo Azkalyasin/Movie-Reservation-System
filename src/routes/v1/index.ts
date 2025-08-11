7006bbe96379410f66952e715d8b229b1a5e7a50
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-08-11 18:18:32 +0700

    handle auth and movie

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
 import routerAuth from './auth';
 import routerMovie from './movie'
-import { validJwt } from '../../middlewares/authenticateJWT';
-import { isAdmin } from '../../middlewares/authorizeAdmin';
+
 
 const router = Router();
 
--- src/routes/v1/movie.ts
+++ src/routes/v1/movie.ts
@@ -3,6 +3,7 @@ import { createMovie } from "../../controller/v1/Movie/createMovie";
 import { updateMovie } from "../../controller/v1/Movie/updateMovie";
 import { getMovieById } from "../../controller/v1/Movie/getMovieById";
 import { getAllMovie } from "../../controller/v1/Movie/getAllMovie";
+import { deleteMovie } from "../../controller/v1/Movie/deleteMovie";
 import { validJwt } from '../../middlewares/authenticateJWT';
 import { isAdmin } from '../../middlewares/authorizeAdmin';
 
@@ -12,6 +13,8 @@ const router = Router();
 router.get("/",getAllMovie )
 router.get("/:id", getMovieById)
 router.post("/create",validJwt,isAdmin, createMovie)
-router.put("/:id",validJwt,isAdmin, updateMovie)
+router.put("/update/:id",validJwt,isAdmin, updateMovie)
+router.delete("/delete/:id", validJwt, isAdmin, deleteMovie)
+
 
 export default router
